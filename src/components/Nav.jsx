2c80bd368270bba543c3c8bbdfd5e80df9e5fa3c
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-02-25 16:05:47 +0530

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Nav = () => {
+
+    
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
      
@@ -38,6 +40,24 @@ const Nav = () => {
 
     <div className='flex items-center gap-6'>
         <img src={assets.search_icon} alt="search-icon" className='w-5 cursor-pointer' />
+        
+        <div className='group relative'>
+                  <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="profile_icon" />
+                  <div className='group-hover:block hidden absolute right-0 pt-4'>
+                      <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-white text-gray-500 rounded shadow-md'>
+                          <p className='cursor-pointer hover:text-black'>My Profile</p>
+                          <p className='cursor-pointer hover:text-black'>Orders</p>
+                          <p className='cursor-pointer hover:text-black'>Logout</p>
+                      </div>
+                  </div>
+        </div>
+
+
+        <Link to='/cart' className='relative'>
+        <img src={assets.cart_icon} alt="cart-icon" className='w-5 min-w-5' />
+        <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[-8px]'>10</p>
+        </Link>
+        <img src={assets.menu_icon} alt="" className='w-5 cursor-pointer sm:hidden' />
 
     </div>
 
