ed40db183a4955c95850a2c8f0f9bcad466527a4
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-03-20 10:50:30 +0530

    orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
 const Orders = () => {
+
+  const { products, currency } = useContext(ShopContext);
   return (
-    <div>Orders</div>
+
+    <div className='border-t pt-16'> 
+    <div className='text-2xl'>
+      <Title text1={'My '} text2={'Orders'} />
+
+    </div>
+
+
+    <div>
+      {
+        products.slice(1,4).map((item, index) => (
+         
+          <div key={index} className='py-4 border-t border-b text-gary-700 flex   md: flex-row md:items-center md:justify-between gap-4 '>
+            <div className='flex itens-start gap-6 text-sm'>
+              <img className='w-16 sm:w-20' src={item.image[0]} alt="" />
+
+
+            </div>
+
+          </div>
+        ))
+      }
+    </div>
+    </div>
   )
 }
 
